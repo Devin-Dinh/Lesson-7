c4c02fb862f3a23a663b066c7dcfeeea9a5c494c
Devin-Dinh at codea-scm <Devin-Dinh@codea-scm.aws.mapote.com>
2016-11-07 16:08:21 +0000

    Initial

---
+++ tabs/Main.lua
@@ -0,0 +1,25 @@
+-- Lesson 7
+
+--Created by: Devin Dinh
+--Created on: Nov-2016
+--Created for ICS2O
+--This program displays te company logo
+
+-- Use this function to perform your initial setup
+function setup()
+    print("Hello World!")
+end
+
+-- This function gets called once every frame
+function draw()
+    -- This sets a dark background color 
+    background(255, 226, 0, 255)
+    
+    --This sets the line thickness
+    strokeWidth(5)
+    
+    --Do your drawing here
+    
+    sprite("Dropbox:Blue Pause Button", WIDTH/2, HEIGHT/2, 500, 500)
+
+end
